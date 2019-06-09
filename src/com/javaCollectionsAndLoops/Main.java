61e435a1e949a496b78a06ba2e7db0f944e7f09a
Nick Fonseca <tester.nick79@gmail.com>
2019-06-09 14:35:58 -0700

    HandsOnLesson3

--- src/com/javaCollectionsAndLoops/Main.java
+++ src/com/javaCollectionsAndLoops/Main.java
@@ -5,5 +5,24 @@ public class Main {
     public static void main(String[] args) {
 	// testing git
         System.out.println("Hello Test");
+
+        // arrays
+
+        String[] bandMembers = {"Bobby", "Frank", "Leroy", "Paul"};
+        // first item in array
+        System.out.println(bandMembers[3]);
+
+        // list of food items
+        String[] foodList = new String[4];
+            foodList[0] = "beer";
+            foodList[1] = "milk";
+            foodList[2] = "eggs";
+            foodList[3] = "garbage bags";
+            foodList[1] = "goat milk";
+
+            System.out.println(foodList[1]);
+
+            int[] fastestSpeeds = {120, 100, 122, 188};
+            System.out.println(fastestSpeeds[2]);
     }
 }
